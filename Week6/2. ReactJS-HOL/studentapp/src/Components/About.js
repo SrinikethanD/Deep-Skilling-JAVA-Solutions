9932b3de6ab959a15828093141239a5c89d25729
creators05 <hariharan63141@gmail.com>
2025-07-24 22:50:05 +0530

    Adding Week 6 Solutions

---
+++ Week6/2. ReactJS-HOL/studentapp/src/Components/About.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+export class About extends Component{
+render(){return(
+        <div>
+        <h4>Welcome to the About Page of the Student Management Portal</h4>
+        </div>
+)
+    }
+}
---
+++ Week6/2. ReactJS-HOL/studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React,{Component} from "react";
+export class Contact extends Component{
+    render(){
+        return(
+        <div>
+        <h4>Welcome to the Contact Page of the Student Management Portal</h4></div>
+        );    
+}
+}
